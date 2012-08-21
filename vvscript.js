178ee5cf7e3e1094024816cf049c92cecc2c3ecd
Ken Shimizu <kenshimizu88@gmail.com>
2012-08-20 20:45:23 -0700

    drawer slides in and out

--- vvscript.js
+++ vvscript.js
@@ -1,9 +1,21 @@
 $(document).ready( function() {
 	var $projectName = $(".project-name")
 	var $projectDescriptions = $('.project-description')
+	var $description
 	
-	$projectName.on("click", function() {
-		$projectDescriptions.hide();
-		$("#"+this.innerHTML).show("slide", { direction: "left" }, 500);
+	$projectName.on("click", function(e) {
+        e.preventDefault();
+		var that = this;
+		if($description) {
+			console.log($description);
+			$description.hide("slide", { direction: "left" }, 500);
+			setTimeout(function() {
+				$("#"+that.innerHTML).show("slide", { direction: "left" }, 500);
+			}, 600);
+		} else {
+			$("#"+that.innerHTML).show("slide", { direction: "left" }, 500);
+		}
+		
+		$description = $("#"+this.innerHTML)
 	});
 });
