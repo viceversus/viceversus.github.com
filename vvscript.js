98ee0dd3d3a83a9c638bc627d534588d3757a2ac
Ken Shimizu <kenshimizu88@gmail.com>
2012-08-22 18:51:16 -0700

    Add multiple tabs and slideDown jquery

--- vvscript.js
+++ vvscript.js
@@ -1,13 +1,40 @@
 $(document).ready( function() {
 	var $projectName = $(".project-name")
 	var $projectDescriptions = $('.project-description')
+	var $tabs = $('.tab')
 	var $description
+	var $tab
+	var $active
+	
+	$tabs.on("click", function(e) {
+		e.preventDefault();
+		
+		if($active) {
+			$active.removeClass('active');
+		}
+		
+		$active = $(this)
+		$active.addClass('active');
+
+		var $that = $('#'+$(this).children().html());
+		
+		if($tab) {
+			$tab.slideUp('slow');
+			setTimeout(function() {
+				$that.slideDown('slow');
+			}, 600);
+		} else {
+			$that.slideDown('slow');
+		}
+		
+		$tab = $that;
+		
+	})
 	
 	$projectName.on("click", function(e) {
         e.preventDefault();
 		var that = this;
 		if($description) {
-			console.log($description);
 			$description.hide("slide", { direction: "left" }, 500);
 			setTimeout(function() {
 				$("#"+that.innerHTML).show("slide", { direction: "left" }, 500);
